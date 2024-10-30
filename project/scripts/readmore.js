5655d20ba7f73d2124be73df0506a0e21887b8de
codewith-ese <ese24002@byui.edu>
2024-10-30 15:47:24 +0100

    abc

--- project/scripts/readmore.js
+++ project/scripts/readmore.js
@@ -1,10 +1,16 @@
-document.getElementById('readMoreBtn').addEventListener('click', function() {
-    var moreContent = document.getElementById('moreContent');
-    if (moreContent.style.display === "none" || moreContent.style.display === "") {
-        moreContent.style.display = "block";
-        this.textContent = "Read Less"; // Change button text
+// Get all toggle buttons
+var toggleButtons = document.getElementsByClassName("toggle-button");
+var j;
+
+// Add click event listener to each button
+for (j = 0; j < toggleButtons.length; j++) {
+  toggleButtons[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var toggleContent = this.nextElementSibling;
+    if (toggleContent.style.maxHeight) {
+      toggleContent.style.maxHeight = null;
     } else {
-        moreContent.style.display = "none";
-        this.textContent = "Read More"; // Change button text back
-    }
-});
+      toggleContent.style.maxHeight = toggleContent.scrollHeight + "px";
+    } 
+  });
+}
